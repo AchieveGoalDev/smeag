b97abe4cb94be0b5fc05834b62fbe797a4a6ea99
Justin <thejasumasa@gmail.com>
2023-09-25 17:53:09 +0900

    set up sveltekit, tailwind, and other plugins TODO enable static

---
+++ src/lib/layout/SectionWrapper.svelte
@@ -0,0 +1,13 @@
+<script lang="ts">
+	export let params: {
+		themeColor: string;
+	};
+
+	function handleThemeColor(themColor: string) {
+		return 'bg-orange-50 h-[50px]';
+	}
+</script>
+
+<div class={handleThemeColor(params.themeColor)}>
+	<slot />
+</div>
---
+++ src/routes/+page.svelte
@@ -0,0 +1,11 @@
+<script lang="ts">
+	import SectionWrapper from '$lib/layout/SectionWrapper.svelte';
+</script>
+
+<h1 class="font-bold bg-green-500">Welcome to SvelteKit</h1>
+
+<SectionWrapper params={{ themeColor: 'derp' }}>
+	<p class="text-blue">Does TW work?</p>
+</SectionWrapper>
+
+<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
