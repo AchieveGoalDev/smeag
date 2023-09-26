a2b3a10b115c35d85dd202c87b47a8605169badf
Jasumasa <thejasumasa@gmail.com>
2023-09-26 16:19:28 +0900

    Create universal button component and define its parameters. NEXT create ghost button

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,5 +1,30 @@
 <script lang="ts">
 	import SectionWrapper from '$lib/layout/SectionWrapper.svelte';
+
+	import ColorButton from '$lib/components/ColorButton.svelte';
+
+	let testButtons = [
+		{
+			color: 'orange',
+			link: '#',
+			function: null
+		},
+		{
+			color: 'purple',
+			link: '#',
+			function: null
+		},
+		{
+			color: 'green',
+			link: '#',
+			function: null
+		},
+		{
+			color: 'blue',
+			link: '#',
+			function: null
+		}
+	];
 </script>
 
 <h1 class="font-bold bg-green-500">Welcome to SvelteKit</h1>
@@ -7,5 +32,11 @@
 <SectionWrapper params={{ themeColor: 'derp' }}>
 	<p class="text-blue">Does TW work?</p>
 </SectionWrapper>
+
+<div class="h-[200px] bg-green-50 flex flex-row place-content-around items-center">
+	{#each testButtons as button}
+		<ColorButton params={button}>詳細はこちら</ColorButton>
+	{/each}
+</div>
 
 <p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
